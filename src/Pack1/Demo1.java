6e10578073602a793587428f5f8e323c3c18f162
Santosh Aireddy <Santosh Aireddy@LAPTOP-OBLDTTSS>
2018-08-04 11:35:50 +0530

    commit 2

--- src/Pack1/Demo1.java
+++ src/Pack1/Demo1.java
@@ -1,32 +1,10 @@
+package Pack1;
+
 public class Demo1
 {
 	public static void main (String[] args) throws InterruptedException
 	{
-	System.setProperty("webdrive.gecko.driver", "./softwares/geckodriver.exe");
 	
-	//Step 1
-	WebDriver driver=new FirefoxDriver();
-	
-	//Step 2
-	driver.get("https://www.bluestone.com/");
-	
-	//Step 3
-	driver.findElement(By.id("open-label")).click();
-	
-	//Step 4
-	driver.findElement(By.id("name")).sendKeys("Santosh223");
-	
-	//Step 5
-	driver.findElement(By.id("email")).sendKeys("santosh1234@gmail.com");
-	
-	//Step 6
-	driver.findElement(By.id("container_4_4")).sendKeys("7412689630");
-	
-	//Step 7
-	driver.findElement(By.xpath("//input[@type='submit']")).click();
-	
-	//Step 8
-	//driver.findElement(By.id());
 	
 	}
 }
